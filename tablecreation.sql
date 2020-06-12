7c7ea3c1a159eea8558308a9d392759102da44d3
mt464 <54955071+mt464@users.noreply.github.com>
2020-06-12 18:27:28 -0400

    Add files via upload

--- tablecreation.sql
+++ tablecreation.sql
@@ -1,9 +1,10 @@
 CREATE TABLE BankAccounts (
 	PersonID int,
-	AccountNumber int(8),
+	AccountNumber int(12),
 	FirstName varchar(25),
 	LastName varchar(25),
 	Amount double(13,2),
 	Recent DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	AccountCreated DATETIME DEFAULT CURRENT_TIMESTAMP
+	PRIMARY KEY (PersonID)
 );
