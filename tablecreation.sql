9780a3dd1e52fcbf1a88d54a9f445e24549c00a0
mt464 <54955071+mt464@users.noreply.github.com>
2020-06-12 17:38:44 -0400

    Add files via upload

---
+++ tablecreation.sql
@@ -0,0 +1,9 @@
+CREATE TABLE BankAccounts (
+	PersonID int,
+	AccountNumber int(8),
+	FirstName varchar(25),
+	LastName varchar(25),
+	Amount double(13,2),
+	Recent DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	AccountCreated DATETIME DEFAULT CURRENT_TIMESTAMP
+);
