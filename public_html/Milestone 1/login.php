aa5328c76c4b8a19e54ca0dbaa76ee161b234ef6
mt464 <54955071+mt464@users.noreply.github.com>
2020-06-13 07:47:24 -0400

    Update login.php

--- public_html/Milestone 1/login.php
+++ public_html/Milestone 1/login.php
@@ -38,7 +38,6 @@ li a:hover:not(.active) {
   <li><a href="register.php">Register</a></li>
 </ul>
 <h1>Login</h1>
-<p>This is where the users accounts would be shown</p>
 
 <form method="POST">
     <label for="email">Email
