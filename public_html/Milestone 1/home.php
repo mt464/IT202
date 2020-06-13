25c928b4fd50951f633fe5d526bbaa5eb3148f97
mt464 <54955071+mt464@users.noreply.github.com>
2020-06-13 06:32:55 -0400

    Add files via upload

--- public_html/Milestone 1/home.php
+++ public_html/Milestone 1/home.php
@@ -12,8 +12,37 @@ h1{
 	color: blue;
 	margin-left: 50px;
 }
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+li {
+  float: left;
+}
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+li a:hover:not(.active) {
+  background-color: #111;
+}
+.active {
+  background-color: #0000FF;
+}
 </style>
 </head>
 <body>
+<ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li style="float:right"><a class="active" href="#about">Logout</a></li>
+</ul>
 <h1>Welcome to MTBank!</h1>
-<p>Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver. --Ayn Rand</p>
+<p>"Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver." -Ayn Rand</p>
