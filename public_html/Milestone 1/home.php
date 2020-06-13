ae17db87cfc7e13ad8376aef9562b5ae6973f221
mt464 <54955071+mt464@users.noreply.github.com>
2020-06-13 06:37:23 -0400

    Add files via upload

--- public_html/Milestone 1/home.php
+++ public_html/Milestone 1/home.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-echo "Welcome, " . $_SESSION["user"]["email"];
 ?>
 <a href="logout.php">Logout</a>
 <head>
@@ -42,7 +41,7 @@ li a:hover:not(.active) {
   <li><a class="active" href="#home">Home</a></li>
   <li><a href="#news">News</a></li>
   <li><a href="#contact">Contact</a></li>
-  <li style="float:right"><a class="active" href="#about">Logout</a></li>
+  <li style="float:right"><a class="active" href="logout.php">Logout</a></li>
 </ul>
 <h1>Welcome to MTBank!</h1>
 <p>"Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver." -Ayn Rand</p>
