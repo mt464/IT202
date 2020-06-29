f304cc7c591ab156aa34fb5a2079c59c45794efd
mt464 <54955071+mt464@users.noreply.github.com>
2020-06-28 22:13:53 -0400

    Add files via upload

--- public_html/HW5/search.php
+++ public_html/HW5/search.php
@@ -32,7 +32,7 @@ if(isset($search)) {
 			echo $results;
 			foreach($results as $value){
 				echo $value . "<br>";
-				print_r ($value);
+				print_r (get($value,"AccountNumber");
 			}
 			
 		} catch (Exception $e) {
